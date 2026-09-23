ad09225bdfd6646b9d58f35366a0f58c871c7252
riyagalhot48-art <riyagalhot48@gmail.com>
2026-09-23 22:35:15 +0530

    Add files via upload

---
+++ student.js
@@ -0,0 +1,17 @@
+let students=[
+    {
+        id:1,
+        name:"Jasleen",
+        age:18,
+        course:"B.Tech CSE"
+    },
+    {
+        id:2,
+        name:"Rahul",
+        age:19,
+        course:"BCA"
+    }
+];
+
+module.exports=students; 
+
